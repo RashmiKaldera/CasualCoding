43d16e206619319d946ed3f90919fc2a8428d555
RashmiKaldera <kalderadissasekara@uchc.edu>
2026-09-16 14:10:43 -0400

    Arithmetic operations

---
+++ IntroTutorials/arithmetic.py
@@ -0,0 +1,22 @@
+print(10+3) #addition
+print(10/3)
+print (10//3) # integer of division
+print(10%3) # modulus
+print(10**3) # exponential
+#%%
+x = 10
+x += 3 # augmented assignment operator
+print(x)
+
+#Operator precedence
+x = 10 + 3 * 2 # same math concepts parenthesis, exponent, multiplication/division, addition/subtraction
+print(x)
+#%%
+x = 2.9
+print(round(x))
+print(abs(-3)) # absolute returns positive value always
+
+#%%
+import math # module
+print(math.floor(3.14))
+print(math.ceil(3.14))
